aa14317af4e7da06baa5ea6501f2cd680eab4a39
GabeValdivia <gabe@gabeswebcoding.com>
2022-01-09 22:34:20 -0600

    Write calculate function necessary to fetch exchange rate from API and calculate currency chosen

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const currencyEl_one = document.getElById('currency-one');
+const currencyEl_one = document.getElementById('currency-one');
 const amountEl_one = document.getElementById('amount-one');
-const currencyEl_two = document.ementById('currency-two');
+const currencyEl_two = document.getElementById('currency-two');
 const amountEl_two = document.getElementById('amount-two');
 
 const rateEl = document.getElementById('rate');
@@ -8,7 +8,18 @@ const swap = document.getElementById('swap');
 
 //Fetch exchange rates and update the DOM
 function calculate() {
-	
+	const currency_one = currencyEl_one.value;
+	const currency_two = currencyEl_two.value;
+
+	fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+	.then(res => res.json())
+	.then(data => {
+		//console.log(data);
+		const rate = data.rates[currency_two];		
+		rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+		amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+	});
 }
 
 //Event listners
